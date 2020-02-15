252cf672d38594048b9e07031657372fbe485016
abdotaker608 <abdotaker607@gmail.com>
2020-02-15 10:12:21 +0200

    deleted dir from repo

--- social/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'login'
-
-urlpatterns = [
-    path('', views.login, name='login'),
-    path('<str:login_error_404>/', views.login_error, name='login_error')
-]
